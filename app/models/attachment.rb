b6edc911671d2fc594939bb759ff65a81da6fed3
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2015-04-14 04:10:15 +0300

    scopes issue

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,7 @@
 class Attachment < ActiveRecord::Base
   belongs_to :question
 
-  mount_uploader :file, FileUploader
+  validates :file, presence: true
+
+  mount_uploader :file, 'FileUploader'
 end
