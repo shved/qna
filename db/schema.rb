6e3db5779bf7306a3ee298d2d2d0f2f914f337fa
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2015-05-09 19:59:25 +0300

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503192722) do
+ActiveRecord::Schema.define(version: 20150509165726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,12 +22,10 @@ ActiveRecord::Schema.define(version: 20150503192722) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "user_id"
-    t.integer  "score",       default: 0
     t.boolean  "best",        default: false
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
-  add_index "answers", ["score"], name: "index_answers_on_score", using: :btree
   add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
 
   create_table "attachments", force: :cascade do |t|
@@ -69,16 +67,19 @@ ActiveRecord::Schema.define(version: 20150503192722) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "vote"
+    t.integer  "value"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.integer  "votable_id"
     t.string   "votable_type"
+    t.integer  "user_id"
   end
 
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
   add_index "votes", ["votable_id", "votable_type"], name: "index_votes_on_votable_id_and_votable_type", using: :btree
 
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
+  add_foreign_key "votes", "users"
 end
