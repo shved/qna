0f579914c92390deaa38497f5385cc7a0c68ab1c
Vitaly Shvedchenko <vitaly.shvedchenko@gmail.com>
2015-06-22 09:22:21 +0300

    hound issues #1

--- spec/factory_girl/authorizations.rb
+++ spec/factory_girl/authorizations.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :authorization do
     user nil
-provider "MyString"
-uid "MyString"
+		provider "MyString"
+		uid "MyString"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,19 +8,21 @@ RSpec.describe User do
   it { should have_many :authorizations }
 
   describe '.find_for_oauth' do
-  	let!(:user) { create(:user) }
-  	let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
 
   	context 'user already has authorization' do
   		it 'returns the user' do
   			user.authorizations.create(provider: 'facebook', uid: '123456')
   			expect(User.find_for_oauth(auth)).to eq user
-  		end
-  	end
+      end
+    end
 
     context 'user has no authorization' do
       context 'user already exists' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: user.email }) }
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook',
+                                            uid: '123456',
+                                            info: { email: user.email }) }
         it 'does not create new user' do
           expect { User.find_for_oauth(auth) }.to_not change(User, :count)
         end
@@ -42,7 +44,9 @@ RSpec.describe User do
       end
 
       context 'user does not exists' do
-        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456', info: { email: 'new@user.com' }) }
+        let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook',
+                                            uid: '123456',
+                                            info: { email: 'new@user.com' }) }
 
         it 'creates new user' do
           expect { User.find_for_oauth(auth) }.to change(User, :count).by(1)
